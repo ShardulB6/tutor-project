e74160ed488ee38ec0e2d956f15f59d33240fbd2
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-02 20:54:22 -0400

    refactor: remove Header and Footer components from RootDocument

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,5 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-  return <main className="page-wrap px-4 pb-8 pt-14">Hello World</main>;
+  return <div>Hello World</div>;
 }
