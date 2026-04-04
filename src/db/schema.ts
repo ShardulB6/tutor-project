ed4d07319bd6b40faa8b2563bdc3ca03b5ce1557
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-04 16:52:05 -0400

    feat: add account, session, user, and verification tables with relationships; update auth schema and configuration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,12 @@
 import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
+export * from "./auth-schema";
+
 export const todos = sqliteTable("todos", {
   id: integer({ mode: "number" }).primaryKey({
     autoIncrement: true,
   }),
-  
   title: text().notNull(),
   createdAt: integer("created_at", { mode: "timestamp" }).default(sql`(unixepoch())`),
   updatedAt: integer("updated_at", { mode: "timestamp" })
