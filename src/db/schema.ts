a8ecb8cafb9566c681107e5ae4459380fc06d27a
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-05 01:55:08 -0400

    refactor: update schema to replace todos with users table; adjust relationships and primary key types

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,33 +1,27 @@
 import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
-
+import crypto from "node:crypto";
+import * as authSchema from "./auth-schema";
 export * from "./auth-schema";
-
-export const users = sqliteTable("todos", {
-  id: integer({ mode: "number" }).primaryKey({
-    autoIncrement: true,
-  }),
-  title: text().notNull(),
-});
 
 export const notebooks = sqliteTable("notebook", {
   title: text().notNull(),
-  id: integer({ mode: "number" }).primaryKey({
-    autoIncrement: true,
-  }),
-  notebookID: integer("notebookID").references(() => users.id),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  userID: integer("userID").references(() => authSchema.user.id),
   createdAt: integer("created_at", { mode: "timestamp" }).default(sql`(unixepoch())`),
   updatedAt: integer("updated_at", { mode: "timestamp" })
     .default(sql`(unixepoch())`)
     .$onUpdate(() => sql`(unixepoch())`),
 });
 
-export const message = sqliteTable("message", {
+export const threads = sqliteTable("threads", {
   title: text().notNull(),
-  id: integer({ mode: "number" }).primaryKey({
-    autoIncrement: true,
-  }),
-  messageID: integer("messageID").references(() => notebooks.id),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  notebookID: integer("notebookID").references(() => notebooks.id),
   createdAt: integer("created_at", { mode: "timestamp" }).default(sql`(unixepoch())`),
   updatedAt: integer("updated_at", { mode: "timestamp" })
     .default(sql`(unixepoch())`)
@@ -35,10 +29,10 @@ export const message = sqliteTable("message", {
 });
 
 export const messages = sqliteTable("messages", {
-  id: integer({ mode: "number" }).primaryKey({
-    autoIncrement: true,
-  }),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
   roles: text().notNull(),
   message: text().notNull(),
-  messageID: integer("messageID").references(() => message.id),
+  threadID: integer("threadID").references(() => threads.id),
 });
