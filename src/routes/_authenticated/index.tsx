026d4785726d1b83822ae1d4b83e2f28c6379575
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-13 18:55:03 -0400

    feat: implement Notebooks route with Create Notebook button and layout

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,9 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { createNotebook } from "#/lib/functions/notebooks.functions";
 
 export const Route = createFileRoute("/_authenticated/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/index"!</div>;
+  return (
+    <div className="p-4">
+      <div>
+        <h1 className="text-2xl font-bold">Notebooks</h1>
+        <button
+          onClick={() => {
+            alert("Create notebook");
+          }}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Create Notebook
+        </button>
+      </div>
+      <div>
+        <div className="border rounded-sm mb-4 mt-4 w-30 h-30"></div>
+      </div>
+    </div>
+  );
 }
