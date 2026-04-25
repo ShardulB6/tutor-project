ba3044ee539fd84839f6acf8cfe2541a4f028ec2
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-24 23:16:23 -0400

    feat: add getChatMessages and createChatMessage functions for chat functionality

--- src/lib/functions/threads.functions.ts
+++ src/lib/functions/threads.functions.ts
@@ -1,3 +1,4 @@
+import { createServerFn } from "@tanstack/react-start";
 import { ensureSession } from "../auth/auth.functions";
 import { db } from "#/db";
 import { NotebooksTable } from "#/db/schema";
@@ -5,11 +6,18 @@ import { createUpdateSchema, createInsertSchema } from "drizzle-zod";
 
 import { eq, and } from "drizzle-orm";
 import z from "zod";
-import { convertToModelMessages, createGateway, streamText, } from 'ai';
-import type { UIMessage } from 'ai'
 
 
+export const getChatMessages = createServerFn({ method: "GET" }).handler(async () => {
+    const session = await ensureSession();
+    const messagesResult = db
+      .select()
+      .from(NotebooksTable)
+      .where(eq(NotebooksTable.userID, session.user.id));
 
+    return messagesResult;
+});
 
+export const createChatMessage = createServerFn({ method: "POST" })
 
 
