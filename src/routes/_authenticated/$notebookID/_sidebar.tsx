dd74496f4ccad8a4bf6712add35fd92f57db1797
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-26 21:53:53 -0400

    refactor: restructure sidebar component layout for improved readability and organization

--- src/routes/_authenticated/$notebookID/_sidebar.tsx
+++ src/routes/_authenticated/$notebookID/_sidebar.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { AppSidebar } from "@/components/ui/MySpecialUI/ChatSidebar";
-import { SidebarProvider } from "#/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "#/components/ui/sidebar";
 
 export const Route = createFileRoute("/_authenticated/$notebookID/_sidebar")({
   component: RouteComponent,
@@ -9,16 +9,15 @@ export const Route = createFileRoute("/_authenticated/$notebookID/_sidebar")({
 function RouteComponent() {
   const { notebookID } = Route.useParams();
   return (
-    <div>
-      <h3>{notebookID}</h3>
-      <div className="flex flex-row h-screen">
-        <SidebarProvider>
-          <AppSidebar />
-          <div className="flex-1">
-            <Outlet />
-          </div>
-        </SidebarProvider>
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <SidebarInset>
+          <h3>{notebookID}</h3>
+          <Outlet />
+        </SidebarInset>
+      </main>
+    </SidebarProvider>
   );
 }
