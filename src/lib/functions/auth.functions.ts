ab3c4c6da0a4685fef0e33700dd3008f0aaf7305
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-30 01:02:55 -0400

    feat: implement ensureMessage function and refactor auth functions for improved modularity

---
+++ src/lib/functions/auth.functions.ts
@@ -0,0 +1,41 @@
+import {  createServerOnlyFn } from "@tanstack/react-start";
+import { ensureSession } from "../auth/auth.functions";
+import { db } from "#/db";
+import { NotebooksTable, ThreadsTable, type MessageId, type NotebookId, type ThreadId } from "#/db/schema";
+
+import { and, eq } from "drizzle-orm";
+import z from "zod";
+
+
+export const ensureNotebook = createServerOnlyFn(async (notebookID: NotebookId) => {
+  const session = await ensureSession();
+  const notebooksResult = await db.query.NotebooksTable.findFirst({
+    where: and(eq(NotebooksTable.id, notebookID), eq(NotebooksTable.userID, session.user.id)),
+  });
+  if (notebooksResult === undefined) {
+    throw Error("unauthorized");
+  }
+
+  return notebooksResult;
+});
+
+export const ensureThread = createServerOnlyFn(async (threadID: ThreadId) => {
+  const session = await ensureSession();
+  const threadResult = await db.query.ThreadsTable.findFirst({
+    where: and(eq(ThreadsTable.id, threadID)),
+    with: {
+      notebook: true,
+    },
+  });
+  if (threadResult === undefined || threadResult.notebook.userID !== session.user.id) {
+    throw Error("unauthorized");
+  }
+  return threadResult;
+});
+
+export const ensureMessage = createServerOnlyFn(async (messageID: MessageId) =>{
+    const session = await ensureSession
+    const messageResult = await db.query.ThreadsTable.findFirst({
+        
+    })
+})
--- src/lib/functions/threads.functions.ts
+++ src/lib/functions/threads.functions.ts
@@ -1,36 +1,11 @@
-import { createServerFn, createServerOnlyFn } from "@tanstack/react-start";
+import { createServerFn, } from "@tanstack/react-start";
 import { ensureSession } from "../auth/auth.functions";
 import { db } from "#/db";
-import { NotebooksTable, ThreadsTable, type NotebookId, type ThreadId } from "#/db/schema";
-
+import { NotebooksTable, ThreadsTable, } from "#/db/schema";
+import { ensureNotebook, ensureThread } from "./auth.functions";
 import { and, eq } from "drizzle-orm";
 import z from "zod";
 
-const ensureNotebook = createServerOnlyFn(async (notebookID: NotebookId) => {
-  const session = await ensureSession();
-  const notebooksResult = await db.query.NotebooksTable.findFirst({
-    where: and(eq(NotebooksTable.id, notebookID), eq(NotebooksTable.userID, session.user.id)),
-  });
-  if (notebooksResult === undefined) {
-    throw Error("unauthorized");
-  }
-
-  return notebooksResult;
-});
-
-const ensureThread = createServerOnlyFn(async (threadID: ThreadId) => {
-  const session = await ensureSession();
-  const threadResult = await db.query.ThreadsTable.findFirst({
-    where: and(eq(ThreadsTable.id, threadID)),
-    with: {
-      notebook: true,
-    },
-  });
-  if (threadResult === undefined || threadResult.notebook.userID !== session.user.id) {
-    throw Error("unauthorized");
-  }
-  return threadResult;
-});
 
 export const createThread = createServerFn({ method: "POST" })
   .inputValidator(z.object({ title: z.string(), notebookID: z.string().brand<"NotebookId">() }))
