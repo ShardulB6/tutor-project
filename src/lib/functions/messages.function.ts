2b2214d2ac41474d01f630b5acd791191abbf198
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-04-30 00:52:15 -0400

    feat: add messages function file with initial imports and setup

---
+++ src/lib/functions/messages.function.ts
@@ -0,0 +1,8 @@
+import { createServerFn, createServerOnlyFn } from "@tanstack/react-start";
+import { ensureSession } from "../auth/auth.functions";
+import { db } from "#/db";
+import { NotebooksTable, ThreadsTable, type NotebookId, type ThreadId } from "#/db/schema";
+
+import { and, eq } from "drizzle-orm";
+import z from "zod";
+
