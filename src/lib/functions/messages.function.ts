6212bcc4dc3938b8f40f521850c931dea8a1b18a
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-05-02 14:12:20 -0400

    feat: refactor createMessage function and update imports for improved structure

--- src/lib/functions/messages.function.ts
+++ src/lib/functions/messages.function.ts
@@ -1,9 +1,27 @@
 import { createServerFn, createServerOnlyFn } from "@tanstack/react-start";
-import { ensureSession } from "../auth/auth.functions";
+import { env } from "../env";
 import { db } from "#/db";
-import { NotebooksTable, ThreadsTable, type NotebookId, type ThreadId } from "#/db/schema";
+import { MessagesTable, NotebooksTable, ThreadsTable} from "#/db/schema";
 
 import { and, eq } from "drizzle-orm";
 import z from "zod";
 import { ensureNotebook, ensureThread, ensureMessage } from "./auth.functions";
+
+
+
+export const createMessage = createServerFn({ method: "POST" })
+    .inputValidator(z.object({ message: z.string(), threadID: z.string().brand<"ThreadId">() }))
+    .handler(async ({ data }) => {
+        await ensureThread(data.threadID);
+        await db.insert(MessagesTable).values({
+            message: data.message,
+            roles: "user",
+            threadID: data.threadID,
+        });
+
+        // add AI response generation here
+        
+
+        return { success: true };
+    });
 
