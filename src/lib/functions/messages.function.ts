9bd1eda8b7b45ca1d8dd71e939386e2495ec5bff
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-05-02 22:14:33 -0400

    feat: implement AI response generation in createMessage function

--- src/lib/functions/messages.function.ts
+++ src/lib/functions/messages.function.ts
@@ -6,8 +6,12 @@ import { MessagesTable, NotebooksTable, ThreadsTable} from "#/db/schema";
 import { and, eq } from "drizzle-orm";
 import z from "zod";
 import { ensureNotebook, ensureThread, ensureMessage } from "./auth.functions";
+import { createGateway, streamText } from "ai";
 
-
+async function getAPIKey(){
+    const AI_key = env.AI_GATEWAY_API_KEY;
+    return AI_key;
+}
 
 export const createMessage = createServerFn({ method: "POST" })
     .inputValidator(z.object({ message: z.string(), threadID: z.string().brand<"ThreadId">() }))
@@ -19,8 +23,24 @@ export const createMessage = createServerFn({ method: "POST" })
             threadID: data.threadID,
         });
 
+
         // add AI response generation here
-        
+        const API_KEY = await getAPIKey();
+
+        const gateway = createGateway({apiKey: API_KEY});
+
+        const { textStream } = streamText({
+            model: "meta/llama-3.1-70b",
+            prompt: "what are cats?"
+        })
+
+        for await (const text of textStream) {
+            await db.insert(MessagesTable).values({
+                message: text,
+                roles: "assistant",
+                threadID: data.threadID,
+            });
+        }               
 
         return { success: true };
     });
