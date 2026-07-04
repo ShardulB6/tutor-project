71aa028c301a87620bc1d13b7b82ce00dff21145
shardul <83481569+ShardulB6@users.noreply.github.com>
2026-07-03 20:42:00 -0400

    feat: implement file upload handler and display uploaded files in panel component

--- src/routes/_authenticated/$notebookID/_sidebar/_panel.tsx
+++ src/routes/_authenticated/$notebookID/_sidebar/_panel.tsx
@@ -13,12 +13,28 @@ export const Route = createFileRoute("/_authenticated/$notebookID/_sidebar/_pane
 });
 
 function RouteComponent() {
+  const { files } = Route.useLoaderData();
+
+  const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {};
+
   return (
     <ResizablePanelGroup orientation="horizontal">
       <ResizablePanel defaultSize="20%" minSize="10%">
         <div className="flex h-24 w-full flex-col items-center justify-center gap-3 px-4">
           <span className="font-semibold">Files</span>
-          <input type="file" accept=".pdf" className="block w-full max-w-40 text-sm" />
+          <input
+            type="file"
+            accept=".pdf"
+            className="block w-full max-w-40 text-sm"
+            onChange={handleFileUpload}
+          />
+          <div>
+            <ul>
+              {files.map((file) => (
+                <li key={file.id}>{file.id}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </ResizablePanel>
       <ResizableHandle />
